ab7dbc0073af2df079994a3990d68a0df6b56817
leeroydon1994 <leeroydon1994@gmail.com>
2020-09-17 13:08:54 +0800

    HTML layout accomplished. Font control added, pen width control amended

--- javascript/color-picker.js
+++ javascript/color-picker.js
@@ -1,12 +1,12 @@
 // iro.js setup
-var boxPicker = new iro.ColorPicker("#picker", {
+var colorPicker = new iro.ColorPicker("#picker", {
     width: 200,
     colors: [
       "rgb(255, 0, 0)",
       "rgb(0, 255, 0)",
     ],
-    borderWidth: 1,
-    borderColor: "#fff",
+    borderWidth: 2,
+    borderColor: "#ccc",
     layout: [
       {
         component: iro.ui.Wheel,
@@ -14,16 +14,36 @@ var boxPicker = new iro.ColorPicker("#picker", {
       {
         component: iro.ui.Slider,
         options: {
-          sliderType: 'value'
+          sliderType: 'saturation'
         }
       },
       {
         component: iro.ui.Slider,
         options: {
-          sliderType: 'alpha'
+          sliderType: 'value'
         }
       }
       
     ]
 });
 
+// Showing primary and secondary colors
+colorPicker.on('color:change', function(color) {
+  // if the first color changed
+  if (color.index === 0) {
+    // log the color index and hex value
+    // console.log(color.index, color.hexString);
+    console.log(colorPicker.colors[0].hexString);
+
+    // Change the preview box
+    $(".primary-color").css("background-color", color.hexString);
+  } else if (color.index === 1) {
+    // log the color index and hex value
+    // console.log(color.index, color.hexString);
+    console.log(colorPicker.colors[1].hexString);
+
+    // Change the preview box
+    $(".secondary-color").css("background-color", color.hexString);
+  }
+
+});
